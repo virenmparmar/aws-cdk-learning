37adeed18d75d4a59601302c715de801f2c51bc0
virenmparmar <virenmparmar@gmail.com>
2025-06-12 14:00:25 -0400

    Added getitem

--- src/resolvers/getNoteResolver.js
+++ src/resolvers/getNoteResolver.js
@@ -1,5 +1,16 @@
+import { util } from "@aws-appsync/utils";
+
 export function request(ctx) {
-  return { operation: "Scan" };
+  const {
+    arguments: { id },
+  } = ctx;
+  if (!id) {
+    util.error("Missing required field: name", "BadRequest");
+  }
+  return {
+    operation: "GetItem",
+    key: util.dynamodb.toMapValues({ id }),
+  };
 }
 
 export function response(ctx) {

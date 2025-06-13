e43e9dfcfde657f71a5020ec0cb558bdf8fe5608
virenmparmar <virenmparmar@gmail.com>
2025-06-13 15:08:34 -0400

    Added JSON stringify

--- src/resolvers/createUpdateNoteResolver.js
+++ src/resolvers/createUpdateNoteResolver.js
@@ -11,7 +11,7 @@ export function request(ctx) {
     message.operation = "CreateNote";
   }
 
-  const encodedMessage = message;
+  const encodedMessage = JSON.stringify(message);
   return {
     version: "2018-05-29",
     method: "POST",
